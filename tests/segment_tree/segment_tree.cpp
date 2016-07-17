e86da7bc8a42de5b4825e7b70e1948e3deca28a8
Pavel Korozevtsev <paxakor@yandex.ru>
2016-07-17 22:30:50 +0300

    segment_tree tested

---
+++ tests/segment_tree/segment_tree.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "segment_tree/segment_tree.hpp"
+
+int main(int argc, char const *argv[]) {
+  int n;
+  std::cin >> n;
+  std::vector<int> numbers(n);
+  for (auto& x : numbers) {
+    std::cin >> x;
+  }
+  SegmentTree<int> st(numbers.begin(), numbers.end());
+  int m;
+  std::cin >> m;
+  for (int i = 0; i < m; ++i) {
+    int l, r;
+    std::cin >> l >> r;
+    std::cout << st.get(l, r + 1) << std::endl;
+  }
+  return 0;
+}
---
+++ tests/segment_tree/test.py
@@ -0,0 +1,22 @@
+from random import randint
+
+f = open('test', 'w')
+a = open('ans', 'w')
+n = 1000
+print(n, file = f)
+v = list()
+for i in range(n):
+  x = randint(-100, 100)
+  v.append(x)
+  print(x, file = f)
+print(file = f)
+
+# m = randint(10, 1000)
+m = 10000
+print(m, file = f)
+for i in range(m):
+  l = randint(0, n - 1)
+  r = randint(0, n - 1)
+  l, r = min(l, r), max(l, r)
+  print(l, r, file = f)
+  print(sum(v[l:r+1]), file = a)
