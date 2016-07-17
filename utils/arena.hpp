bc760703fcf33373b329043dc86cda914d3c38c3
Pavel Korozevtsev <paxakor@yandex.ru>
2016-07-17 22:29:59 +0300

    simple arena added

---
+++ utils/arena.hpp
@@ -0,0 +1,24 @@
+// Copyright 2016, Pavel Korozevtsev.
+
+#pragma once
+
+#include <vector>
+
+template <typename Type>
+class Arena {
+public:
+  ~Arena() {
+    for (auto ptr : _storage) {
+      delete[] ptr;
+    }
+  }
+
+  Type* get(size_t size) {
+    Type* ptr = new Type[size];
+    _storage.push_back(ptr);
+    return ptr;
+  }
+
+private:
+  std::vector<Type*> _storage;
+};
